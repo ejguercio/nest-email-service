b44de7545ace7cea889557c5620fbed8bfadc33d
Edgar Juan Guercio <jguercio@live.com.ar>
2024-03-21 14:06:53 -0300

    ADD message in root

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { MailModule } from './mail/mail.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { throttlerConfig } from './config/throttler.config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ThrottlerModule.forRoot([throttlerConfig]), MailModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [
     {
       provide: APP_GUARD,
