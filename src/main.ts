b527b0aa66b41df34ed61a4ab70e1015df71e918
Edgar Juan Guercio <jguercio@live.com.ar>
2024-03-24 16:01:26 -0300

    ADD eneable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.ALLOW_ORIGIN || '*',
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true, // Importante si estás usando cookies/sessiones
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const PORT = process.env.PORT || 3000;
